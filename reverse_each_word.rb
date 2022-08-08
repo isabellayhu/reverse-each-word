ecbae00e96a2f889c96d450d6c0a94c1e6a6e2d8
Isabella <isabella.y.hu@gmail.com>
2022-08-08 19:44:44 +1000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,6 +4,6 @@ def reverse_each_word (sentence)
     # sentence_array.each do |word|
     #     new_sentence << word.reverse
     # end
-    sentence_array.collect {|word| new_sentence << word.reverse}
-    new_sentence.join(" ")
+    # new_sentence.join(" ")
+    sentence_array.collect {|word| word.reverse}.join(" ")
 end
